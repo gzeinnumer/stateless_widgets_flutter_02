bb16bf0b473b6afee423654389ef56588b2f62d4
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2019-06-15 11:46:06 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,51 +17,11 @@ class MyStatelessWidget extends StatelessWidget{
         child: new Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            // new Text("Hy"),
-            // new Text("My Name"),
-            // new Text("Is Zein")
-            new MyCard(
-              title: new Text("I Love Flutter", style: new TextStyle(fontSize: 20.0)),
-              icon: new Icon(Icons.favorite, size: 40.0, color: Colors.redAccent,)
-            ),
-            new MyCard(
-              title: new Text("I Love Zein", style: new TextStyle(fontSize: 20.0),),
-              icon: new Icon(Icons.person, size: 40.0, color: Colors.brown )
-            ),
-            new MyCard(
-              title: new Text("I see you", style: new TextStyle(fontSize: 20.0),),
-              icon: new Icon(Icons.visibility, size:40.0, color:Colors.blue)
-            )
+            new Text("Hy"),
+            new Text("My Name"),
+            new Text("Is Zein")
+            
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class MyCard extends StatelessWidget{
-  MyCard({this.title, this.icon});
-
-  final Widget title;
-  final Widget icon;
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Container(
-      padding: new EdgeInsets.only(bottom: 20.0),
-      child: new Card(
-        child: new Container(
-          padding: new EdgeInsets.all(15.0),
-          child: new Column(
-            children: <Widget>[
-              // new Text("I Love FLutter"),
-              // new Icon(Icons.favorite)
-
-              this.title,
-              this.icon
-            ],
-          ),
         ),
       ),
     );
